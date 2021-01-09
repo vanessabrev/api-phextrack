00de2a0f0b92643fe66f8a64985f4b38ae052a1a
Josiel <josielsifa@gmail.com>
2021-01-09 13:26:55 -0300

    Adicionando enpoint para listar usuarios

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::resource('user', 'UserController'); // FIXME: ASSIM CRIA TODO O PADRAO REST
+
+Route::get('user', 'Api\\UserController@index');
