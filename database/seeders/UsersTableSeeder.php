d5e2f32f51e1e2560da9b450cc8cd2cfabc267b5
Josiel <josielsifa@gmail.com>
2021-01-09 10:32:32 -0300

    adicionando configuração para tabela de usuarios

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class UsersTableSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $now = date('Y-m-d H:i:s');
+    DB::table('users')->insert([
+      [
+        'name' => 'Josiel Faria',
+        'email' => 'developerjosiel@gmail.com',
+        'email_verified_at' => $now,
+        'password' => '1234',
+        'remember_token' =>  Str::random(10),
+        'created_at' => $now,
+        'updated_at' => $now,
+      ],
+      [
+        'name' => 'Teste Teste',
+        'email' => 'teste@teste.com',
+        'email_verified_at' => $now,
+        'password' => '$2y$10$sdyi9WPf1UNbFrGoGcpW.uL.ua5sDrWOF2YQLpqbip1i5iKUsghky',
+        'remember_token' => Str::random(10),
+        'created_at' => $now,
+        'updated_at' => $now,
+      ],
+    ]);
+  }
+}
