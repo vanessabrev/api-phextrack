b635415fe75ce6200e037a69f2788c41bb149f4c
Josiel <josielsifa@gmail.com>
2021-01-25 18:16:29 -0300

    Adicionando pretty routes para listagems das rotas disponiveis

--- config/app.php
+++ config/app.php
@@ -176,6 +176,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+         /*
+         * Listar Rotas na url /routes...
+         */
+        PrettyRoutes\ServiceProvider::class,
     ],
 
     /*
