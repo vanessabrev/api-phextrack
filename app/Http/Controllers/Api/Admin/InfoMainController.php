bda2f9ac179c11b958e322cb3bc338069dca6a46
Josiel <josielsifa@gmail.com>
2021-02-03 01:14:23 -0300

    Adicionando endpoint para atualizar e consultar info main

---
+++ app/Http/Controllers/Api/Admin/InfoMainController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\InfoMain;
+use Illuminate\Http\Request;
+
+
+class InfoMainController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $itens = InfoMain::all();
+    return response()->json($itens);
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    try {
+      $info = InfoMain::find($id);
+      if ($info) {
+        $info->update($request->all());
+        return response()->json(["message" => 'Informacoes foram atualizadas'], 200);
+      } else {
+        return response()->json(["message" => 'ID não encontrado no banco de dados'], 500);
+      }
+    } catch (\Exception $e) {
+      return response()->json(["error" => '$e->getCode()']);
+    }
+  }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,15 +16,24 @@ use Illuminate\Support\Facades\Route;
 
 // FIXME: ASSIM CRIA TODO O PADRAO REST
 // Route::resource('user', 'UserController');
-// Route::resource('usuarios', 'Api\\UserController@index')->names('user)->parameters['usuarios' => 'user']; // 13:34  https://www.youtube.com/watch?v=_fDd_RKKWzk
+// Route::resource('usuarios', 'Api\\UserControlle')->names('user)->parameters['usuarios' => 'user']; // 13:34  https://www.youtube.com/watch?v=_fDd_RKKWzk
 
+// LOGIN FORA DO APIJWT PARA CHAMADA SEM TOKEN
 // Route::post('auth/login', 'Api\\AuthController@login'); // somente para uso do admin
-// Route::group(['middleware' => 'apiJwt',], function ($router) { // somente para uso do admin
+
+// TODO: colocar no apiJwt
+// Route::resource('admin/info-home', 'Api\\Site\\InfoHomeController');
+Route::get('admin/info-home', 'Api\\Admin\\InfoMainController@index');
+Route::post('admin/info-home/{id}', 'Api\\Admin\\InfoMainController@update');
+
+
+Route::group(['middleware' => 'apiJwt',], function ($router) { // somente para uso do admin
+  // CONTROLE DE ACESSO
   // Route::get('user', 'Api\\UserController@index');
   // Route::post('auth/logout', 'Api\\AuthController@logout');
   // Route::post('auth/refresh', 'Api\\AuthController@refresh');
   // Route::post('me', 'AuthController@me'); // verificar o uso
-// });
+});
 
 
 /*
